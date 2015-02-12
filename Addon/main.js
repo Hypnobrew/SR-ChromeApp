d433dc435acca0db4ce70b59da001b6603b0ff92
Cristoffer Andersson <cristoffer@ezy.se>
2015-02-12 15:18:12 +0100

    Added choose channel

--- Addon/main.js
+++ Addon/main.js
@@ -1,16 +1,19 @@
 //http://api.sr.se/api/v2/channels?liveaudiotemplateid=2&audioquality=hi
-
 //http://api.sr.se/api/v2/channels?liveaudiotemplateid=0&audioquality=hi
 
 $(document).ready(function(){
+
     var backgroundTask = chrome.extension.getBackgroundPage();
     var shouldAutoplay = $("#autoplay");
     shouldAutoplay.prop('checked', backgroundTask.getShouldAutoplay());
 
     backgroundTask.createAudioPlayer();
 
+    fetchChannels();
+
     $("#start_btn").click(function(){
-        backgroundTask.setAudioUrl('http://sverigesradio.se/topsy/direkt/164.mp3');
+        var url = $('#channels').val();
+        backgroundTask.setAudioUrl(url);
         backgroundTask.start();
     });
 
@@ -26,4 +29,21 @@ $(document).ready(function(){
             backgroundTask.setShouldAutoplay(false);
         }
     });
+
+    function fetchChannels() {
+        $.ajax({
+            url: "http://api.sr.se/api/v2/channels?liveaudiotemplateid=2&audioquality=hi&format=json",
+        }).done(function (data) {
+            handleData(data.channels);
+        });
+    }
+
+    function handleData(data) {
+        var channels = $('#channels');
+        $.each(data, function(i, item) {
+            channels.append(new Option(item.name, item.liveaudio.url));
+        });
+    };
 });
+
+
